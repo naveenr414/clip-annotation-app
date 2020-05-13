2946c4a5e342ca19a7f83350531824f9863027e8
naveenr414 <nraman1@DESKTOP-NK1NFCF.localdomain>
2020-05-13 13:32:55 -0400

    Increased the time till users expire

--- quel-api/quel/security.py
+++ quel-api/quel/security.py
@@ -19,7 +19,7 @@ def create_access_token(*, data: dict, expires_delta: timedelta = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=60*24*2*7)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
         to_encode, security_config.SECRET_KEY, algorithm=security_config.ALGORITHM
